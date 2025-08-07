050d2ddc09b760113899b4b7c611be690a278751
GalaxyWheel001 <sosambusdiana@outlook.com>
2025-08-07 14:33:29 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["*.preview.same-app.com"],
+  reactStrictMode: true,
   images: {
     unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
       "ext.same-assets.com",
-      "ugc.same-assets.com",
+      "ugc.same-assets.com"
     ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "source.unsplash.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ext.same-assets.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
