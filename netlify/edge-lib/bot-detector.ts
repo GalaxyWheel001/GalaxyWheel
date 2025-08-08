cb07a2a5e6fef9a161c41aa5b365d00ca56ae2b2
GalaxyWheel001 <sosambusdiana@outlook.com>
2025-08-08 11:35:43 +0000

    Delete netlify directory

--- netlify/edge-lib/bot-detector.ts
+++
@@ -1,8 +0,0 @@
-export function isBot(userAgent: string): boolean {
-  if (!userAgent) return false;
-
-  const botRegex =
-    /bot|crawl|spider|facebookexternalhit|slurp|mediapartners|adsbot|bingpreview|twitterbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|crawler|python-requests|axios|wget|fetch/i;
-
-  return botRegex.test(userAgent);
-}
